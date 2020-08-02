5047497a41bc264164890a09c5a9a4574f497948
John Flavian <flavianomeokwe@gmail.com>
2020-08-02 17:27:32 +0100

    Passed second challenge, wrote a short information.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 
 
-function MyApp() {
-  render(
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  )
 
+function MyInfo() {
+  return(
+    <div>
+      <h1>John Flavian</h1>
+      <p>I'm a curious web dev that is learning React. </p>
+      <p>Places that i'll like to visit are:</p>
+      <ul>
+        <li>United States of America</li>
+        <li>Brazil</li>
+        <li>Rome</li>
+      </ul>
+    </div>
+  )
 }
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MyInfo />
+  </React.StrictMode>, document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
